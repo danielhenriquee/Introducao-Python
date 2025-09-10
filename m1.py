a6f6186b860fadd77b18836f51ca18fe99abd069
Daniel Henrique <138157786+danielhenriquee@users.noreply.github.com>
2025-09-10 17:35:57 -0300

    Update m1.py

--- m1.py
+++ m1.py
@@ -1,5 +1,5 @@
-# Daniel Henrique da Silva - Código de pessoa: 8109346 - Número de matrícula: 23.2.2001
-# Guilherme Melo           - Código de pessoa: 8576076 - Número de matrícula: 26.1.0379
+# Daniel Henrique da Silva   - Código de pessoa: 8109346 - Número de matrícula: 23.2.2001
+# Guilherme Melo             - Código de pessoa: 8576076 - Número de matrícula: 26.1.0379
 # Leonardo Pinheiro de Souza - Código de pessoa: 8557802 - Número de matrícula: 25.2.7332
 
 # Lista de produtos, preços e carrinho # Itens são associados pelos índices
